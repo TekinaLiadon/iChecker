23385bbba39bda33ec4292e217578f5fd181f823
Tekina <nm0321@yandex.ru>
2025-09-09 22:52:16 +0300

    minor update

--- 04-shared/utils/checkTime.ts
+++ 04-shared/utils/checkTime.ts
@@ -3,5 +3,5 @@ export default (): boolean => {
     var now = new Date();
     var hour = now.getHours();
     var day = now.getDay();
-    return hour >= 8 && hour <= 17 && day >= 1 && day <= 5;
+    return hour >= 12 && hour <= 21 && day >= 1 && day <= 5;
 }
